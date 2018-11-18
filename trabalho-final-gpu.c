84e05aba51ec648eb6bf5034ba8e283474cf5a4e
vinigarcia <vingarciasc@gmail.com>
2018-11-18 17:00:49 -0200

    Refactoring, calculating mean

--- trabalho-final-gpu.c
+++ trabalho-final-gpu.c
@@ -28,9 +28,28 @@ using myClock = std::chrono::high_resolution_clock;
       fprintf(stderr,"Erro no arquivo '%s', linha %i: %s.\n",__FILE__, __LINE__,cudaGetErrorString(err)); \
       exit(EXIT_FAILURE); } } 
 
+__device__ void differentiate_X(double* X, double* X_prime, double drag_coeff, double drag_coeff_a, double drag_coeff_b, 
+    double drag_coeff_vc, double drag_coeff_vs, double magnus_coeff, double gravity) {
+
+    double vx = X[3];
+    double vy = X[4];
+    double vz = X[5];
+    double velocity = sqrt(vx * vx + vy * vy + vz * vz);
+    double sqrt_vx_vy = sqrt(vx * vx + vy * vy);
+    double drag_coeff_v = drag_coeff_a + (drag_coeff_b / (1 + exp(((velocity - drag_coeff_vc) / drag_coeff_vs))));
+
+    X_prime[0] = X[3];
+    X_prime[1] = X[4];
+    X_prime[2] = X[5];
+    X_prime[3] = - (drag_coeff) * ((drag_coeff_v * velocity * vx) + ((magnus_coeff * velocity * vy) / (sqrt_vx_vy)));
+    X_prime[4] = - (drag_coeff) * ((drag_coeff_v * velocity * vy) - ((magnus_coeff * velocity * vx) / (sqrt_vx_vy)));
+    X_prime[5] = - (drag_coeff) * (drag_coeff_v * velocity * vz) - gravity;
+}
+
 __global__ void calculate(unsigned long seed, curandState *state, double* results, int result_size,
-    double initial_velocity, double timestep, double gravity, double drag_coeff,
-    double drag_coeff_a, double drag_coeff_b, double drag_coeff_vc, double drag_coeff_vs) {
+    double velocity, double timestep, double gravity, double drag_coeff,
+    double drag_coeff_a, double drag_coeff_b, double drag_coeff_vc, double drag_coeff_vs,
+    double object_radius, double object_angular_velocity, double proportionality_constant) {
 
     int id = threadIdx.x;
     curand_init(seed, id, 0, &state[id]);
@@ -44,45 +63,29 @@ __global__ void calculate(unsigned long seed, curandState *state, double* result
     double X_1_E[6];
     double X_1[6];
 
-    double init_vx, init_vy, init_vz;
-    init_vx = initial_velocity * cos(theta) * sin(phi);
-    init_vy = initial_velocity * cos(theta) * cos(phi);
-    init_vz = initial_velocity * sin(theta);
-
-    double drag_coeff_v;
-    drag_coeff_v = drag_coeff_a + (drag_coeff_b / (1 + exp(((initial_velocity - drag_coeff_vc) / drag_coeff_vs))));
-    
-    double init_dvx, init_dvy, init_dvz;
-    init_dvx = - (drag_coeff * drag_coeff_v * initial_velocity * init_vx);
-    init_dvy = - (drag_coeff * drag_coeff_v * initial_velocity * init_vy);
-    init_dvz = - (drag_coeff * drag_coeff_v * initial_velocity * init_vz) - gravity;
+    double vx, vy, vz;
+    vx = velocity * cos(theta) * sin(phi);
+    vy = velocity * cos(theta) * cos(phi);
+    vz = velocity * sin(theta);
+    double sqrt_vx_vy = sqrt(vx * vx + vy * vy);
 
     //X = (x, y, z, vx, vy, vz)
     X[0] = 0;
     X[1] = 0;
     X[2] = 0;
-    X[3] = init_vx;
-    X[4] = init_vy;
-    X[5] = init_vz;
+    X[3] = vx;
+    X[4] = vy;
+    X[5] = vz;
+
+    //drag_coeff_v = Cr(v) = a + b/(1 + exp((v - vc) / vs))
+    double drag_coeff_v = drag_coeff_a + (drag_coeff_b / (1 + exp(((velocity - drag_coeff_vc) / drag_coeff_vs))));
+    //magnus_coeff = Cm * r * w
+    double magnus_coeff = proportionality_constant * object_angular_velocity * object_radius;
 
     //X' = (vx, vy, vz, dvx, dvy, dvz)
-    X_prime[0] = init_vx;
-    X_prime[1] = init_vy;
-    X_prime[2] = init_vz;
-    X_prime[3] = init_dvx;
-    X_prime[4] = init_dvy;
-    X_prime[5] = init_dvz;
-
-    double vx, vy, vz, velocity;
+    differentiate_X(X, X_prime, drag_coeff, drag_coeff_a, drag_coeff_b, drag_coeff_vc, drag_coeff_vs, magnus_coeff, gravity);
 
     for (int i = 0; i < 500; i++) {
-        //=== Inicializando passo
-        vx = X[3];
-        vy = X[4];
-        vz = X[5];
-        velocity = sqrt(vx * vx + vy * vy + vz * vz);
-        
-        drag_coeff_v = drag_coeff_a + (drag_coeff_b / (1 + exp(((velocity - drag_coeff_vc) / drag_coeff_vs))));
         // if (id == 0) {
         //     printf(" time: %lf,\n", i * timestep);
         //     printf(" x*: %lf,\n", X[0]);
@@ -104,12 +107,7 @@ __global__ void calculate(unsigned long seed, curandState *state, double* result
         X_1_E[5] = X[5] + (timestep * X_prime[5]);
 
         //X'e(n+1) armazenado em Xe(n+1)
-        X_1_E[0] = X_1_E[3];
-        X_1_E[1] = X_1_E[4];
-        X_1_E[2] = X_1_E[5];
-        X_1_E[3] = - (drag_coeff * drag_coeff_v * velocity * vx);
-        X_1_E[4] = - (drag_coeff * drag_coeff_v * velocity * vy);
-        X_1_E[5] = - (drag_coeff * drag_coeff_v * velocity * vz) - gravity;
+        differentiate_X(X_1_E, X_1_E, drag_coeff, drag_coeff_a, drag_coeff_b, drag_coeff_vc, drag_coeff_vs, magnus_coeff, gravity);
 
         //X(n+1) = X + (Δt/2)*(X'(n) + X'e(n+1))
         X_1[0] = X[0] + (timestep * ((X_prime[0] + X_1_E[0]) / 2.0));
@@ -122,7 +120,7 @@ __global__ void calculate(unsigned long seed, curandState *state, double* result
         if (X_1[1] > 20.0) {
             double proportion = ((20.0 - X[1]) / (X_1[1] - X[1]));
 
-            results[id * result_size] = X[0] + (X_1[0] - X[0]) * proportion;
+            results[id * result_size + 0] = X[0] + (X_1[0] - X[0]) * proportion;
             results[id * result_size + 1] = X[1] + (X_1[1] - X[1]) * proportion;
             results[id * result_size + 2] = X[2] + (X_1[2] - X[2]) * proportion;
             results[id * result_size + 3] = X[3] + (X_1[3] - X[3]) * proportion;
@@ -145,14 +143,8 @@ __global__ void calculate(unsigned long seed, curandState *state, double* result
         X[3] = X_1[3];
         X[4] = X_1[4];
         X[5] = X_1[5];
-
-        //X'(n) <- X'(n+1)
-        X_prime[0] = X[3];
-        X_prime[1] = X[4];
-        X_prime[2] = X[5];
-        X_prime[3] = init_dvx;
-        X_prime[4] = init_dvy;
-        X_prime[5] = init_dvz;
+        
+        differentiate_X(X, X_prime, drag_coeff, drag_coeff_a, drag_coeff_b, drag_coeff_vc, drag_coeff_vs, magnus_coeff, gravity);
     }
 }
 
@@ -185,32 +177,38 @@ int main(int argc, char** argv) {
     double drag_coeff_vc = 7.5;
     double drag_coeff_vs = 0.707;
 
-    // double object_angular_velocity = 6.28;
-    // double proportionality_constant = 1;
+    double object_angular_velocity = 6.28;
+    double proportionality_constant = 1;
 
     CUDA_SAFE_CALL(cudaMalloc((void**) &d_results, n_bytes));
 
     curandState *devStates;
     CUDA_SAFE_CALL(cudaMalloc((void**) &devStates, n_blocos * n_threads * sizeof(curandState)));
 
-    calculate<<<n_blocos, n_threads>>>(time(NULL), devStates, d_results, result_size, initial_velocity, timestep, gravity, drag_coeff, drag_coeff_a, drag_coeff_b, drag_coeff_vc, drag_coeff_vs);
+    calculate<<<n_blocos, n_threads>>>(time(NULL), devStates, d_results, result_size, initial_velocity, 
+        timestep, gravity, drag_coeff, drag_coeff_a, drag_coeff_b, drag_coeff_vc, drag_coeff_vs,
+        object_radius, object_angular_velocity, proportionality_constant);
     CUDA_SAFE_CALL(cudaGetLastError());
 
     CUDA_SAFE_CALL(cudaMemcpy(h_results, d_results, n_bytes, cudaMemcpyDeviceToHost));
 
+    double t_acc = 0.0;
+    double x_acc = 0.0;
+    double y_acc = 0.0;
+    double z_acc = 0.0;
+
     for (int i = 0; i < n_threads; i++) {
-        printf("RESULT[%d]: [\n", i);
-
-        printf(" time: %lf,\n", h_results[i * result_size + 6]);
-        printf(" x*: %lf,\n", h_results[i * result_size + 0]);
-        printf(" y*: %lf,\n", h_results[i * result_size + 1]);
-        printf(" z*: %lf,\n", h_results[i * result_size + 2]);
-        printf(" vx*: %lf,\n", h_results[i * result_size + 3]);
-        printf(" vy*: %lf,\n", h_results[i * result_size + 4]);
-        printf(" vz*: %lf,\n", h_results[i * result_size + 5]);
-
-        printf("]\n");
+        t_acc += h_results[i * result_size + 6];
+        x_acc += h_results[i * result_size + 0];
+        y_acc += h_results[i * result_size + 1];
+        z_acc += h_results[i * result_size + 2];
     }
+
+    printf("RESULTS: \n");
+    printf(" E[tf] = %lf\n", (t_acc / n_threads));
+    printf(" E[x*] = %lf\n", (x_acc / n_threads));
+    printf(" E[y*] = %lf\n", (y_acc / n_threads));
+    printf(" E[z*] = %lf\n", (z_acc / n_threads));
 
 	return 0;
 }
