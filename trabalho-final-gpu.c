9d972c1cc035481117604b4be05b8e447cc0023c
vinigarcia <vingarciasc@gmail.com>
2018-11-25 16:07:31 -0200

    Adding sequential implementation

--- trabalho-final-gpu.c
+++ trabalho-final-gpu.c
@@ -11,6 +11,7 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <random>
 #include <sys/time.h>
 
 using std::chrono::duration;
@@ -127,9 +128,104 @@ __global__ void calculate(unsigned long seed, curandState *state, double* result
     }
 }
 
+void seq_differentiate_X(double* X_src, double* X_dst, double drag_coeff, double drag_coeff_a, double drag_coeff_b, 
+    double drag_coeff_vc, double drag_coeff_vs, double magnus_coeff, double gravity, int use_extra_forces) {
+
+    double vx = X_src[3];
+    double vy = X_src[4];
+    double vz = X_src[5];
+    double velocity = sqrt(vx * vx + vy * vy + vz * vz);
+    double sqrt_vx_vy = sqrt(vx * vx + vy * vy);
+    double drag_coeff_v = drag_coeff_a + (drag_coeff_b / (1 + exp(((velocity - drag_coeff_vc) / drag_coeff_vs))));
+
+    X_dst[0] = vx;
+    X_dst[1] = vy;
+    X_dst[2] = vz;
+    X_dst[3] = (- (drag_coeff) * ((drag_coeff_v * velocity * vx) + ((magnus_coeff * velocity * vy) / (sqrt_vx_vy)))) * use_extra_forces;
+    X_dst[4] = (- (drag_coeff) * ((drag_coeff_v * velocity * vy) - ((magnus_coeff * velocity * vx) / (sqrt_vx_vy)))) * use_extra_forces;
+    X_dst[5] = - gravity + (- (drag_coeff) * (drag_coeff_v * velocity * vz)) * use_extra_forces;
+}
+
+void seq_calculate(int result_size, double* results, int n_bytes, int use_extra_forces,
+    double velocity, double timestep, double gravity, double theta_center, double theta_std_dev, double phi_center, 
+    double phi_std_dev, int dim_to_stop, double value_to_stop, bool stop_on_lesser, double drag_coeff,
+    double drag_coeff_a, double drag_coeff_b, double drag_coeff_vc, double drag_coeff_vs,
+    double object_radius, double object_angular_velocity, double proportionality_constant,
+    int iterations) {
+
+    double X[6];
+    double X_prime[6];
+    double X_1_E[6];
+
+    std::default_random_engine generator;
+    for (int k = 0; k < iterations; k++) {
+        std::normal_distribution<double> theta_distribution(theta_center, theta_std_dev);
+        std::normal_distribution<double> phi_distribution(phi_center, phi_std_dev);
+
+        double theta = (theta_distribution(generator)) * (PI / 180.0);
+        double phi = (phi_distribution(generator)) * (PI / 180.0);
+
+        //X = (0, 0, 0, init_vx, init_vy, init_vz)
+        X[0] = 0;
+        X[1] = 0;
+        X[2] = 0;
+        X[3] = velocity * cos(theta) * sin(phi);
+        X[4] = velocity * cos(theta) * cos(phi);
+        X[5] = velocity * sin(theta);
+
+        //drag_coeff_v = Cr(v) = a + b/(1 + exp((v - vc) / vs))
+        double drag_coeff_v = drag_coeff_a + (drag_coeff_b / (1 + exp(((velocity - drag_coeff_vc) / drag_coeff_vs))));
+        //magnus_coeff = Cm * r * w
+        double magnus_coeff = proportionality_constant * object_angular_velocity * object_radius;
+
+        for (int i = 0; i < 1000000; i++) {
+            //X' = (vx, vy, vz, dvx, dvy, dvz)
+            seq_differentiate_X(X, X_prime, drag_coeff, drag_coeff_a, drag_coeff_b, drag_coeff_vc, drag_coeff_vs, magnus_coeff, gravity, use_extra_forces);
+
+            //Xe(n+1) = X(n) + Δt*X'(n)
+            X_1_E[0] = X[0] + (timestep * X_prime[0]);
+            X_1_E[1] = X[1] + (timestep * X_prime[1]);
+            X_1_E[2] = X[2] + (timestep * X_prime[2]);
+            X_1_E[3] = X[3] + (timestep * X_prime[3]);
+            X_1_E[4] = X[4] + (timestep * X_prime[4]);
+            X_1_E[5] = X[5] + (timestep * X_prime[5]);
+
+            //X'e(n+1) armazenado em Xe(n+1)
+            seq_differentiate_X(X_1_E, X_1_E, drag_coeff, drag_coeff_a, drag_coeff_b, drag_coeff_vc, drag_coeff_vs, magnus_coeff, gravity, use_extra_forces);
+
+            //X(n+1) = X + (Δt/2)*(X'(n) + X'e(n+1))
+            X[0] += (timestep * ((X_prime[0] + X_1_E[0]) / 2.0));
+            X[1] += (timestep * ((X_prime[1] + X_1_E[1]) / 2.0));
+            X[2] += (timestep * ((X_prime[2] + X_1_E[2]) / 2.0));
+            X[3] += (timestep * ((X_prime[3] + X_1_E[3]) / 2.0));
+            X[4] += (timestep * ((X_prime[4] + X_1_E[4]) / 2.0));
+            X[5] += (timestep * ((X_prime[5] + X_1_E[5]) / 2.0));
+
+            if ((((X[dim_to_stop] - value_to_stop) < 0) && stop_on_lesser) || (((X[dim_to_stop] - value_to_stop) > 0) && !stop_on_lesser)) {
+                //=== Interpolando resultados
+                double end_value = X[dim_to_stop];
+                double previous_value = X[dim_to_stop] - (timestep * ((X_prime[dim_to_stop] + X_1_E[dim_to_stop]) / 2.0));
+                double proportion = 1 - (end_value - value_to_stop) / (end_value - previous_value);
+
+                results[k * result_size + 0] = X[0] + ((timestep * ((X_prime[0] + X_1_E[0]) / 2.0))) * (proportion - 1);
+                results[k * result_size + 1] = X[1] + ((timestep * ((X_prime[1] + X_1_E[1]) / 2.0))) * (proportion - 1);
+                results[k * result_size + 2] = X[2] + ((timestep * ((X_prime[2] + X_1_E[2]) / 2.0))) * (proportion - 1);
+                results[k * result_size + 3] = X[3] + ((timestep * ((X_prime[3] + X_1_E[3]) / 2.0))) * (proportion - 1);
+                results[k * result_size + 4] = X[4] + ((timestep * ((X_prime[4] + X_1_E[4]) / 2.0))) * (proportion - 1);
+                results[k * result_size + 5] = X[5] + ((timestep * ((X_prime[5] + X_1_E[5]) / 2.0))) * (proportion - 1);
+
+                double stoptime = ((i-1) * timestep + (proportion) * timestep);
+                results[k * result_size + 6] = stoptime;
+
+                break;
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     if (argc < 5) {
-        printf("Usage:\n  <file> <n_blocos> <n_threads> <timestep> <verbose? [0 | 1]>\n");
+        printf("Usage:\n  <file> <n_blocos> <n_threads> <timestep> <verbose? [0 | 1]> <measure sequential? [0 | 1]>\n");
         printf("Optional:\n  <velocity> <theta> <theta std dev> <phi> <phi std dev> <dim to stop [0 | 1 | 2]>");
         printf("<value to stop> <stop on lesser or greater? [lesser 0 | greater 1]> <use extra forces? [0 | 1]>\n");
         printf("Aborting.\n");
@@ -140,15 +236,16 @@ int main(int argc, char** argv) {
     int n_threads = atoi(argv[2]);
     double timestep = atof(argv[3]);
     bool verbose = atoi(argv[4]);
-    double initial_velocity = (argc > 5) ? atof(argv[5]) : 25.0;
-    double theta = (argc > 6) ? atof(argv[6]) : 15;
-    double theta_std_dev = (argc > 7) ? atof(argv[7]) : 3;
-    double phi = (argc > 8) ? atof(argv[8]) : 10;
-    double phi_std_dev = (argc > 9) ? atof(argv[9]) : 3;
-    int dim_to_stop = (argc > 10) ? atoi(argv[10]) : 1;
-    double value_to_stop = (argc > 11) ? atoi(argv[11]) : 20;
-    bool stop_on_lesser = (argc > 12) ? atoi(argv[12]) : false;
-    int use_extra_forces = (argc > 13) ? atoi(argv[13]) : 1;
+    bool should_measure_seq = (argc > 5) ? atoi(argv[5]) : false;
+    double initial_velocity = (argc > 6) ? atof(argv[6]) : 25.0;
+    double theta = (argc > 7) ? atof(argv[7]) : 15;
+    double theta_std_dev = (argc > 8) ? atof(argv[8]) : 3;
+    double phi = (argc > 9) ? atof(argv[9]) : 10;
+    double phi_std_dev = (argc > 10) ? atof(argv[10]) : 3;
+    int dim_to_stop = (argc > 11) ? atoi(argv[11]) : 1;
+    double value_to_stop = (argc > 12) ? atoi(argv[12]) : 20;
+    bool stop_on_lesser = (argc > 13) ? atoi(argv[13]) : false;
+    int use_extra_forces = (argc > 14) ? atoi(argv[14]) : 1;
 
     int result_size = 7;
     int n_bytes = sizeof(double) * n_threads * n_blocos * result_size;
@@ -159,6 +256,7 @@ int main(int argc, char** argv) {
 
     double inicio, fim;
     double tempo_media;
+    double tempo_seq;
     cudaEvent_t start, stop;
 
     //Initialization
@@ -248,7 +346,49 @@ int main(int argc, char** argv) {
 
     printf("DURATION: \n");
     printf("  Kernel duration = %f seg \n", delta_eventos / 1000);
-    printf("  Mean calc duration = %f seg \n", tempo_media);
+    printf("  Mean calc duration = %f seg \n\n", tempo_media);
+    
+    if (should_measure_seq) {
+        double* seq_results = (double*) malloc(n_bytes);
+
+        GET_TIME(inicio);
+        seq_calculate(result_size, seq_results, n_bytes, use_extra_forces, initial_velocity, timestep, gravity,
+            theta, theta_std_dev, phi, phi_std_dev, dim_to_stop, value_to_stop, stop_on_lesser,
+            drag_coeff, drag_coeff_a, drag_coeff_b, drag_coeff_vc, drag_coeff_vs,
+            object_radius, object_angular_velocity, proportionality_constant, n_blocos * n_threads);
+        GET_TIME(fim);
+        tempo_seq = fim - inicio;
+
+        t_acc = 0;
+        x_acc = 0;
+        y_acc = 0;
+        z_acc = 0;
+        goals = 0;
+        
+        for (int i = 0; i < n_blocos * n_threads; i++) {
+            t_acc += seq_results[i * result_size + 6];
+            x_acc += seq_results[i * result_size + 0];
+            y_acc += seq_results[i * result_size + 1];
+            z_acc += seq_results[i * result_size + 2];
+
+            if (seq_results[i * result_size + 2] > 0.0   && seq_results[i * result_size + 2] < 2.44 &&
+                seq_results[i * result_size + 2] > -3.66 && seq_results[i * result_size + 2] < 3.66) {
+                goals++;
+            }
+        }
+
+        printf("RESULTS (SEQUENTIAL): \n");
+        printf("  E[tf] = %lf\n", (t_acc / (n_threads * n_blocos)));
+        printf("  E[x*] = %lf\n", (x_acc / (n_threads * n_blocos)));
+        printf("  E[y*] = %lf\n", (y_acc / (n_threads * n_blocos)));
+        printf("  E[z*] = %lf\n", (z_acc / (n_threads * n_blocos)));
+        printf("  goals = %d\n", goals);
+        printf("  iterations = %d\n", (n_threads * n_blocos));
+        printf("  goal rate = %lf%%\n", ((double) goals / ((n_threads * n_blocos)) * 100.0));
+
+        printf("DURATION: \n");
+        printf("  Sequential duration = %f seg \n", tempo_seq);
+    }
 
     cudaFree(devStates);
     cudaFree(d_results);
